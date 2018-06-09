54233855b0899ab66dd96ba9271460a176239c7d
Salome Lambermont <salome.lambermont@gmail.com>
2018-06-09 15:01:50 +0100

    Create 7 routes and views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,32 @@
 class PostsController < ApplicationController
+  def index
 
+  end
+
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+  
   private
 
   def post_params
