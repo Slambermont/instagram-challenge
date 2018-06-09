2b3d41a659ccf7fc570454d6df86da70a78ac554
Salome Lambermont <salome.lambermont@gmail.com>
2018-06-09 22:17:46 +0100

    User has many posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
   has_attached_file :image, styles: { large: '600x600#', medium: '400x400#' }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
+  belongs_to :user
 end
