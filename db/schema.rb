5687b1977adfbdd2ef6447690de39d49ffe20a44
Salome Lambermont <salome.lambermont@gmail.com>
2018-06-09 20:57:23 +0100

    User can add picture to a post

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_09_134442) do
+ActiveRecord::Schema.define(version: 2018_06_09_190541) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "posts", force: :cascade do |t|
     t.text "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.integer "image_file_size"
+    t.datetime "image_updated_at"
   end
 
 end
