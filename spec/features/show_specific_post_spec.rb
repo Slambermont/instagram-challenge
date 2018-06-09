9e6df382f973a06f858118559b1d8f053a55954f
Salome Lambermont <salome.lambermont@gmail.com>
2018-06-09 16:36:41 +0100

    Links back to homepage from show page

--- spec/features/show_specific_post_spec.rb
+++ spec/features/show_specific_post_spec.rb
@@ -9,4 +9,14 @@ RSpec.feature 'Shows a specific post', type: :feature do
     click_link('Hello, Instagram!')
     expect(current_path).to eq('/posts/1')
   end
+
+  scenario 'links back to homepage' do
+    visit ('/')
+    click_link('New post')
+    fill_in "Message", with: "Hello, Instagram!"
+    click_button "Add a New Post"
+    click_link('Hello, Instagram!')
+    click_link('Back to Homepage')
+    expect(current_path).to eq('/')
+  end
 end
